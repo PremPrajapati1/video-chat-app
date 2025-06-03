cccc4245fa05d72d0849a614883bc349e9ca4b81
PremPrajapati1 <dsangeeta418@gmail.com>
2025-06-03 19:13:12 +0530

    uu

--- server/server.js
+++ server/server.js
@@ -42,5 +42,5 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
