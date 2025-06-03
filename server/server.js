15f71c8f3745e5651fa81a320be5bf6d97cfc972
PremPrajapati1 <dsangeeta418@gmail.com>
2025-06-03 18:34:11 +0530

    Restructured project for Render deploy

---
+++ server/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+app.use(cors());
+
+// Serve static files from React build
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: '*' } });
+
+io.on('connection', (socket) => {
+  socket.on('join-room', ({ roomId, username }) => {
+    socket.join(roomId);
+    socket.to(roomId).emit('user-joined', { id: socket.id, username });
+  });
+
+  socket.on('signal', ({ to, data }) => {
+    io.to(to).emit('signal', { from: socket.id, data });
+  });
+
+  socket.on('chat-message', ({ roomId, username, message }) => {
+    socket.to(roomId).emit('chat-message', { username, message });
+  });
+
+  socket.on('leave-room', ({ roomId }) => {
+    socket.leave(roomId);
+    socket.to(roomId).emit('user-left', socket.id);
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('user-left', socket.id);
+  });
+});
+
+// Send React app for all other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+const PORT = process.env.PORT || 10000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
